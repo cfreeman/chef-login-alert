efac88cfff0f7bc3e1b9b0348d8ca266ca0e4f17
clinton <clinton.freeman@gmail.com>
2018-06-08 12:15:55 +1000

    Fixed script error.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license 		 'The MIT License'
 description      'Email notification on SSH login'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version 		 '0.2.0'
+version 		 '0.3.0'
 
 supports 'ubuntu'
 
